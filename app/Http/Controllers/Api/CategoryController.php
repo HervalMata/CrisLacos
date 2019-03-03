f813898987101fcde29ee82976e1b9b98e843333
HervalMata <hervalmataamparo@gmail.com>
2019-03-03 12:54:23 -0300

    Criando controller de categorias

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace CrisLacos\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use CrisLacos\Http\Controllers\Controller;
+use CrisLacos\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryRequest $request)
+    {
+        $category = Category::create($request->all());
+        $category->refresh();
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->fill($request->all());
+        $category->save();
+
+        return $category;
+
+        //return response([], 204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+       $category->delete();
+
+       return response([], 204);
+    }
+}
