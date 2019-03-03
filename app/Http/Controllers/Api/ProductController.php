87489d1201b2bc21fe54d98291cc4a441f46cae4
HervalMata <hervalmataamparo@gmail.com>
2019-03-03 13:14:35 -0300

    Criando controller de produtos

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace CrisLacos\Http\Controllers\Api;
+
+use CrisLacos\Http\Requests\ProductRequest;
+use Illuminate\Http\Request;
+use CrisLacos\Http\Controllers\Controller;
+use CrisLacos\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Product[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param ProductRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $product = Product::create($request->all());
+        $product->refresh();
+
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Product $product
+     * @return Product
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param ProductRequest $request
+     * @param Product $product
+     * @return Product
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->fill($request->all());
+        $product->save();
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response([], 204);
+    }
+}
