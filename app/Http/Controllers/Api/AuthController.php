ecbcf3dc465ba4605cf9a853d09f62a8b8d762b4
HervalMata <hervalmataamparo@gmail.com>
2019-03-05 15:57:22 -0300

    Protegendo a API - Logout - Pegando usuário autenticado.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace CrisLacos\Http\Controllers\Api;
 
+use CrisLacos\Http\Resources\UserResource;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use CrisLacos\Http\Controllers\Controller;
@@ -23,5 +24,17 @@ class AuthController extends Controller
             response()->json([
                 'error' => \Lang::get('auth.failed')
             ], 400);
+    }
+
+    public function logout(Request $request)
+    {
+        \Auth::guard('api')->logout();
+        return response()->json([], 404);
+    }
+
+    public function me()
+    {
+        $usuario = \Auth::guard('api')->user();
+        return new UserResource($usuario);
     }
 }
