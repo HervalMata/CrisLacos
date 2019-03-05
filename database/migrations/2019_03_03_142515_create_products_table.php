d8fd28a2f03cd803aeb3de9ec22704870781857e
HervalMata <hervalmataamparo@gmail.com>
2019-03-04 23:46:56 -0300

    Removidas configurações das migrations e adicionadas no arquivo database.php

--- database/migrations/2019_03_03_142515_create_products_table.php
+++ database/migrations/2019_03_03_142515_create_products_table.php
@@ -23,9 +23,6 @@ class CreateProductsTable extends Migration
             $table->boolean('active')->default(1);
             $table->timestamps();
             $table->softDeletes();
-            $table->engine = 'InnoDB';
-            $table->charset = 'utf8';
-            $table->collation='utf8_unicode_ci';
         });
     }
 
