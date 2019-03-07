0bfdede03cfbcf9fa891981171c6bd74f03a7379
HervalMata <hervalmataamparo@gmail.com>
2019-03-06 21:22:33 -0300

    Criando events personalizados para o componente de alert

--- angular-app/src/app/components/pages/login/login.component.ts
+++ angular-app/src/app/components/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'login',
@@ -10,35 +10,37 @@ import {Router} from '@angular/router';
 export class LoginComponent implements OnInit {
 
     credentials = {
-        email : 'admin@user.com',
-        password: 'secret'
-    };
+    email : 'admin@user.com',
+    password : 'secret'
+  };
 
-    // ###################################### INFORMAÇÕES IMPORTANTES ########################################
-    // property biding -> [innerHTML]
-    // Símbolo [] - O TS(Type Script) reflete alterações no template | Dados alteram ---> Template
-    // Símbolo () - O Evento reflete alterações no TS(Type Script) | Template alteram ---> Dados
-    // Two way data biding
+    showMessageError = false;
 
-    constructor(private http: HttpClient, private router: Router) { // injeção de dependência automática
+    //###################################### INFORMAÇÕES IMPORTANTES ########################################
+    //property biding -> [innerHTML]
+    //Símbolo [] - O TS(Type Script) reflete alterações no template | Dados alteram ---> Template
+    //Símbolo () - O Evento reflete alterações no TS(Type Script) | Template alteram ---> Dados
+    //Two way data biding
 
-    }
+  constructor(
+      private http: HttpClient,
+      private router: Router
+  ) { //injeção de dependência automática
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+  }
 
-    }
-
-    submit(){
-        // Enviar uma requisição ajax com as credenciais para API
+  submit(){
+        //Enviar uma requisição ajax com as credenciais para API
         this.http.post<any>('http://localhost:8000/api/login', this.credentials)
             .subscribe((data) => {
-                this.router.navigate(['categories/list']);
                 const token = data.token;
                 window.localStorage.setItem('token', token);
                 this.router.navigate(['categories/list']);
-            });
+            }, () => this.showMessageError = true);
 
         return false;
-    }
+  }
 
 }
