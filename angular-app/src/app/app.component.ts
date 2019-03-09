daab564637941ae57efeea58498c129eb283e8dd
HervalMata <hervalmataamparo@gmail.com>
2019-03-09 13:34:16 -0300

    Pegando informações do usuário autenticado.

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import pace from 'pace';
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import pace from 'pace';
 })
 export class AppComponent implements OnInit{
   title = 'app';
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     pace.start({
--- angular-app/src/app/services/auth.service.ts
+++ angular-app/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {tap} from "rxjs/operators";
+import {User} from "../model";
+import {JwtHelperService} from '../../../node_modules/@auth0/angular-jwt';
 
 const TOKEN_KEY = 'cris_lacos_token';
 
@@ -10,7 +12,12 @@ const TOKEN_KEY = 'cris_lacos_token';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  me: User = null;
+
+  constructor(private http: HttpClient) {
+    const token = this.getToken();
+    this.setUserFromToken(token);
+  }
 
   login(user: {email: string, password: string}) : Observable<{token: string}> {
     return this.http.post<{token: string}>('http://localhost:8000/api/login', user)
@@ -23,10 +30,17 @@ export class AuthService {
 
 
   setToken(token: string) {
+      this.setUserFromToken(token);
       window.localStorage.setItem(TOKEN_KEY, token);
   }
 
   getToken() : string | null {
       return window.localStorage.getItem(TOKEN_KEY);
+  }
+
+  private setUserFromToken(token: string) {
+     const decodeToken = new JwtHelperService().decodeToken(token);
+     // @ts-ignore
+      this.me = decodeToken ? {id: decodeToken.sub, name: decodeToken.name, email: decodeToken.email} : null;
   }
 }
