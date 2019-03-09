7881c7c8fea03fa335fa9f32161cde2395298ab1
HervalMata <hervalmataamparo@gmail.com>
2019-03-09 02:18:31 -0300

    Criando método de Login

--- angular-app/src/app/components/pages/login/login.component.ts
+++ angular-app/src/app/components/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'login',
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     //Two way data biding
 
   constructor(
-      private http: HttpClient,
+      private authService: AuthService,
       private router: Router
   ) { //injeção de dependência automática
   }
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
 
   submit(){
         //Enviar uma requisição ajax com as credenciais para API
-        this.http.post<any>('http://localhost:8000/api/login', this.credentials)
+        this.authService.login(this.credentials)
             .subscribe((data) => {
                 const token = data.token;
                 window.localStorage.setItem('token', token);
