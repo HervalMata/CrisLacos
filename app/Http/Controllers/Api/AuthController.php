7bef8f12a5647ee35dd659d75dd0a055ca714f77
HervalMata <hervalmataamparo@gmail.com>
2019-03-13 11:54:22 -0300

    Gerando token JWT depois da verificação Firebase.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,11 @@
 namespace CrisLacos\Http\Controllers\Api;
 
 use CrisLacos\Http\Resources\UserResource;
+use CrisLacos\Models\UserProfile;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use CrisLacos\Http\Controllers\Controller;
+use CrisLacos\Firebase\Auth as FirebaseAuth;
 
 class AuthController extends Controller
 {
@@ -19,11 +21,22 @@ class AuthController extends Controller
 
         $token = \JWTAuth::attempt($credentials);
 
-        return $token ?
-            ['token' => $token] :
-            response()->json([
-                'error' => \Lang::get('auth.failed')
-            ], 400);
+        return $this->responseToken($token);
+    }
+
+    public function loginFirebase(Request $request)
+    {
+        $firebaseAuth = app(FirebaseAuth::class);
+        $user = $firebaseAuth->user($request->token);
+        $user = $firebaseAuth->user($teste);
+        $profile = UserProfile::where('phone_number', $user->phoneNumber)->first();
+        $token = null;
+
+        if ($profile) {
+            $token = \Auth::guard('api')->login($profile->user);
+        }
+
+        return $this->responseToken($token);
     }
 
     public function logout(Request $request)
@@ -42,5 +55,14 @@ class AuthController extends Controller
     {
         $token = \Auth::guard('api')->refresh();
         return ['token' => $token];
+    }
+
+    private function responseToken($token)
+    {
+        return $token ?
+            ['token' => $token] :
+            response()->json([
+                'error' => \Lang::get('auth.failed')
+            ], 400);
     }
 }
--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -9,4 +9,9 @@ class UserProfile extends Model
     protected $fillable = [
         'phone_number'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
